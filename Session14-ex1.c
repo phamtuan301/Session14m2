3ec3fea7ca18fe322394d07f224261c06c3ff657
phamtuan301 <tuan13nt@gmail.com>
2024-12-11 22:32:32 +0700

    Add files via upload

---
+++ Session14-ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];  
+    printf("Nhap chuoi: ");
+    fgets(str, 100, stdin);  
+    printf("Chuoi ban vua nhap la: %s\n", str);
+    printf("Do dai chuoi ban nhap la: %d\n", strlen(str)-1);   
+    return 0;
+}
